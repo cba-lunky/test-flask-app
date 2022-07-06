2f4f67c048935f3d825378a8c5c9bd4dcc32045e
cba-lunky <kelvin.lun@commbank.engineering>
2022-07-06 14:04:05 +1000

    print thingo

--- be2/app.py
+++ be2/app.py
@@ -5,11 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    url = "https://raw.githubusercontent.com/cba-lunky/test-txt/main/test.txt
+    url = "https://raw.githubusercontent.com/cba-lunky/test-txt/main/test.txt"
     page = requests.get(url)
 
+    text += f"This is microservice backend 2 \n {url} \n"
+    text += page.text
+
     if page.status_code == requests.codes.ok:
-        return(page.text)
+        return(text)
     else:
         return "uh oh spaghettios"
 
